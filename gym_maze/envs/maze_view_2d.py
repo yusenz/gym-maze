a87a7803053ba6ab379967d15a016f40bf8f9cbf
Yusen Zhu <yusenz@gatech.edu>
2024-09-24 07:02:05 -0400

    Using display handle to show in the same object

--- gym_maze/envs/maze_view_2d.py
+++ gym_maze/envs/maze_view_2d.py
@@ -17,6 +17,7 @@ class MazeView2D:
         self.clock = pygame.time.Clock()
         self.__game_over = False
         self.__enable_render = enable_render
+        self.__display_handle = display(None, display_id=True)
 
         # Load a maze
         if maze_file_path is None:
@@ -135,8 +136,8 @@ class MazeView2D:
 
             if mode == "human":
                 pygame.display.flip()
-                pygame.image.save(self.screen, "maze.png")
-                display(Image(filename="maze.png"))
+                pygame.image.save(self.screen, "maze.jpeg")
+                self.__display_handle.update(Image("maze.jpeg"))
 
 
             return np.flipud(np.rot90(pygame.surfarray.array3d(pygame.display.get_surface())))
